0869e619d9fa007cc0f68475af0d8fa3d0224d8f
zenkdare <zenkdare@gmail.com>
2020-09-21 15:54:30 -0400

    code stuff

---
+++ crimson_coast_real/Assets/Charting_a_Course.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Charting_a_Course : MonoBehaviour
+{
+    public GameObject pointPrefab;
+    public GameObject linePrefab;
+    public List<Point> points = new List<Point>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Create_point_and_line();
+        }
+    }
+
+    void Create_point_and_line()
+    {
+        print(Input.mousePosition);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit = new RaycastHit();
+        print(EventSystem.current.IsPointerOverGameObject());
+        if (EventSystem.current.IsPointerOverGameObject()==false)    // is the touch on the GUI
+        {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag == "map")
+                {
+                    GameObject instance = Instantiate(pointPrefab, hit.point, Quaternion.identity);
+                    print(instance.transform);
+                    Point marker = instance.GetComponent<Point>();
+                    marker.Setchart(this);
+                    points.Add(marker);
+                    marker.Creation_setup();
+                    if (points.Count > 1)
+                    {
+                        points[points.Count - 2].Added_point(points[points.Count - 1]);
+                    }
+                }
+            }
+        }
+    }
+
+    public void RemoveanItem(Point point)
+    {
+        int index = points.IndexOf(point);
+        if (index == points.Count-1 && index!=0)
+        {
+            points[index - 1].Added_point(points[index-1]);
+        }
+        else if (index > 0)
+        {
+            points[index - 1].Added_point(points[index + 1]);
+        }
+        points.Remove(point);
+        print(points.Count);
+    }
+
+    public void Clear_course()
+    {
+        print("clear is called");
+        while(points.Count>0)
+        {
+            points[0].Clear_point();
+        }
+    }
+}
---
+++ crimson_coast_real/Assets/ManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerScript : MonoBehaviour
+{
+    public CameraScript cam;
+    public GameObject start_town;
+    public GameObject chart_course_button;
+    public GameObject confirm_course_button;
+    public GameObject reset_course_button;
+    public GameObject set_sail_button;
+    public GameObject map;
+    public GameObject courseCharter;
+    public GameObject current_location;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpTown(start_town);
+        current_location = start_town;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetUpTown(GameObject town)
+    {
+        cam.Look_at_Location(town);
+        chart_course_button.SetActive(true);
+        set_sail_button.SetActive(true);
+        current_location = start_town;
+    }
+
+    public void Chart_a_Course()
+    {
+        cam.Look_at_Location(map);
+        chart_course_button.SetActive(false);
+        set_sail_button.SetActive(false);
+        confirm_course_button.SetActive(true);
+        reset_course_button.SetActive(true);
+        courseCharter.SetActive(true);
+    }
+
+    public void Confirm_course()
+    {
+        cam.Look_at_Location(current_location);
+        chart_course_button.SetActive(true);
+        set_sail_button.SetActive(true);
+        confirm_course_button.SetActive(false);
+        reset_course_button.SetActive(false);
+        courseCharter.SetActive(false);
+    }
+
+    public void Clear_course()
+    {
+        Charting_a_Course charter = courseCharter.GetComponent<Charting_a_Course>();
+        charter.Clear_course();
+    }
+}
